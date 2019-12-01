52d8c6cc291fb7f68ebc1be9fed376ce55d0fa14
Nyaimo <nyaimo2005@yahoo.com>
2019-12-01 15:13:53 +0300

    paypal button added

--- src/context.js
+++ src/context.js
@@ -100,7 +100,7 @@ class ProductProvider extends Component {
         
         product.count = product.count -1;
         
-        if(product.count == 0){
+        if(product.count === 0){
             this.removeItem(id);
         }else{
         product.total = product.count * product.price;
